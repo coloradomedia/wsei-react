dc89640527574322e2088f672355f7d405a26695
coloradomedia <piotrek.majkrzak@gmail.com>
2021-03-21 17:33:46 +0100

    basic structure

---
+++ wsei-frameworki/src/components/LeftMenu/LeftMenu.tsx
@@ -0,0 +1,12 @@
+import { FC } from 'React';
+import styled from 'styled-components';
+
+export const LeftMenu: FC = () => {
+  return (
+    <>
+      <div>LeftMenu</div>
+    </>
+  )
+}
+
+export default LeftMenu;
---
+++ wsei-frameworki/src/components/TopBar/TopBar.tsx
@@ -0,0 +1,12 @@
+import { FC } from 'React';
+import styled from 'styled-components';
+
+export const TopBar: FC = () => {
+  return (
+    <>
+      <div>TopBar</div>
+    </>
+  )
+}
+
+export default TopBar;
